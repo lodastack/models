2ac53145efd70acb69bb299a640402544bd2cea2
kun <oiooj@qq.com>
2017-03-07 17:05:54 +0800

    add measurement type for HTTPResponse

--- APIStats.go
+++ APIStats.go
@@ -6,13 +6,16 @@ import (
 
 // HTTPResponse struct
 type HTTPResponse struct {
-	Name                    string 		`json:"name"`
-	Address                 string 		`json:"address"`
-	Body                    string 		`json:"body"`
-	Method                  string 		`json:"method"`
-	ResponseTimeout         string 		`json:"responseTimeout"`
-	ResponseStringMatch     string 		`json:"responseStringMatch"`
-	CompiledStringMatch     *regexp.Regexp
+	Name                string `json:"name"`
+	Interval            string `json:"interval"`
+	MeasurementType     string `json:"measurement_type"`
+	Comment             string `json:"comment"`
+	Address             string `json:"address"`
+	Body                string `json:"body"`
+	Method              string `json:"method"`
+	ResponseTimeout     string `json:"responseTimeout"`
+	ResponseStringMatch string `json:"responseStringMatch"`
+	CompiledStringMatch *regexp.Regexp
 
-	InsecureSkipVerify      bool
+	InsecureSkipVerify bool
 }
