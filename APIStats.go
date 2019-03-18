326fc099cf31922eb09a293d620e37e7fc27181b
kun <oiooj@qq.com>
2019-03-18 11:57:04 +0800

    add API status tags

--- APIStats.go
+++ APIStats.go
@@ -11,6 +11,10 @@ type HTTPResponse struct {
 	MeasurementType     string `json:"measurement_type"`
 	Comment             string `json:"comment"`
 	Address             string `json:"address"`
+	Owner               string `json:"owner"`
+	Level               string `json:"level"`
+	Group               string `json:"group"`
+	PassLine            string `json:"passline"`
 	Body                string `json:"body"`
 	Method              string `json:"method"`
 	ResponseTimeout     string `json:"responseTimeout"`
